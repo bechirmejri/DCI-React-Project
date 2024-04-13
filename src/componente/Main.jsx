499b841adf36dcac2555b62d316c27719d53fa3d
bechir mejri <badjo387@gmail.com>
2024-04-13 19:33:45 +0200

    gif in carousel bearbeitet

--- src/componente/Main.jsx
+++ src/componente/Main.jsx
@@ -8,6 +8,7 @@ export default function Main() {
         backgroundColor: '#ea580c',
       }}
       loop={true} // Aktiviere die Endlosschleife
+      autoplay={true} // Aktiviere das automatische Abspielen
       responsive={{
         // Passe die Anzahl der Slides pro Ansicht basierend auf der Bildschirmgröße an
         "640": {
@@ -21,6 +22,32 @@ export default function Main() {
         },
       }}
     >
+      
+ <img style={{ marginTop: '270px' }}
+        src="https://i.gifer.com/78og.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
+       <img style={{ marginTop: '270px' }}
+        src="https://media.contentapi.ea.com/content/dam/swgoh/common/articles/kit-reveal-jedi-knight-cal-kestis/whirndl-defense.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
+       <img style={{ marginTop: '270px' }}
+        src="https://i.gifer.com/15fp.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
+      <img style={{ marginTop: '270px' }}
+        src="https://wewillblogyoumusic.files.wordpress.com/2015/10/fifa.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
+       <img style={{ marginTop: '270px' }}
+        src="https://i.pinimg.com/originals/73/00/ba/7300ba34decf9956a85cf40b5b69ecee.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
       <img style={{ marginTop: '270px' }}
         src="https://gaming-cdn.com/images/products/9575/616x353/helldivers-2-pc-game-steam-europe-cover.jpg"
         alt="image 1"
@@ -36,6 +63,12 @@ export default function Main() {
         alt="image 3"
         className="w-600 object-cover mx-auto rounded-lg"
       />
+      <img style={{ marginTop: '270px' }}
+        src="https://rocketbeans-fan.de/gifs/GameTwo/TrantMichaRRage2G2_10F.gif"
+        alt="image 4"
+        className="w-600 object-cover mx-auto rounded-lg"
+      />
+
     </Carousel>
   );
 }
