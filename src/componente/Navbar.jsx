60c9fb8825caf87fc0d4bc3b943f2aadf2f3bfc5
bechir mejri <badjo387@gmail.com>
2024-04-18 09:28:59 +0200

    commit leer

--- src/componente/Navbar.jsx
+++ src/componente/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
